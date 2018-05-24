38be5462aaa2f7150ee86e7f90f8eeb438ed7296
lukepettway <luke.pettway@gmail.com>
2018-05-23 22:55:54 -0400

    created basic control structure for fizzbuz

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            Console.WriteLine("Enter a number so we can fizz buzz: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+            int i = 1;
+            while (i <= num)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                i++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
